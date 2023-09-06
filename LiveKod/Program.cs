abc8e159bdec80bc2abb794945085f5299f52533
albin-hedberg <143542598+albin-hedberg@users.noreply.github.com>
2023-09-06 13:37:20 +0200

    qwertrty

--- LiveKod/Program.cs
+++ LiveKod/Program.cs
@@ -106,6 +106,8 @@
 //    Console.WriteLine();
 //}
 
+using System.Numerics;
+
 int[] players = new int[4]; // Spelare 1-4
 int[] scores = new int[4];  // Poäng (har samma index som spelarna)
 int[] places = new int[4];  // Plats (1:a index är spelaren med högst kast, 2:a index är med näst högst, etc.)
@@ -145,40 +147,46 @@ void Score()
 
         // Spara index av spelaren med högst kast på nuvarande position i arrayen 'places'
         places[place] = highestRollIndex;
+
+        // Poäng:
+        // 1:a = 5p
+        // 2:a = 3p
+        // 3:a = 1p
+        // 4:a = 0p
+        switch (place)
+        {
+            case 0:
+                scores[highestRollIndex] += 5;
+                break;
+            case 1:
+                scores[highestRollIndex] += 3;
+                break;
+            case 2:
+                scores[highestRollIndex] += 1;
+                break;
+            case 3:
+                scores[highestRollIndex] += 0;
+                break;
+            default:
+                break;
+        }
+
         // Återställ sedan spelarens kast till 0 för att nästa iteration av loopen ska få fram 2:a & 3:e plats, o.s.v.
         players[highestRollIndex] = 0;
+        highestRollIndex = -1;
         highestRoll = 0;
     }
 
-    // TODO: Loop för att räknu ut poängen
+    for(int i = 0; i < scores.Length; i++)
+    {
+        Console.WriteLine("Player" + (i + 1) + " points: " + scores[i]);
+    }
 }
 
 // "En metod för att avgöra segraren av spelet"
 void Winner()
 {
-    int highestScore = 0;        // Högsta kastet
-    int highestScoreIndex = -1;  // Index av spelare med högst kast
-
-    // Loopa igenom alla platser (First, Second, Third, etc.)
-    for (int place = 0; place < places.Length; place++)
-    {
-        // Loopa igenom alla spelare
-        for (int currentPlayer = 0; currentPlayer < players.Length; currentPlayer++)
-        {
-            // Om det nuvarande kastet är det högsta
-            if (players[currentPlayer] > highestScore)
-            {
-                highestScore = players[currentPlayer];   // Spara värdet av högsta kastet
-                highestScoreIndex = currentPlayer;       // Spara index av spelaren
-            }
-        }
-
-        // Spara index av spelaren med högst poäng på nuvarande position i arrayen 'places'
-        places[place] = highestScoreIndex;
-        // Återställ sedan spelarens poäng till 0 för att nästa iteration av loopen ska få fram 2:a & 3:e plats, o.s.v.
-        players[highestScoreIndex] = 0;
-        highestScore = 0;
-    }
+    Console.WriteLine("Winner: ");
 }
 
 // Main loop för spelet
@@ -188,8 +196,9 @@ for (int round = 0; round < 6; round++)
     Round();
     Score();
 
-    Console.WriteLine("\nPress any key to continue...");
-    Console.ReadKey();
+    Console.WriteLine();
+    //Console.WriteLine("\nPress any key to continue...");
+    //Console.ReadKey();
 }
 
 Winner();
